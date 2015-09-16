100c997b460a6c4a56ce52fbc78012c97a982990
MrHexagon <rickysilk@gmail.com>
2015-09-16 19:29:55 -0400

    fix persistent dividers

--- ionic-ion-autoListDivider.js
+++ ionic-ion-autoListDivider.js
@@ -1,29 +1,37 @@
 angular.module('ionic.ion.autoListDivider',[])
 
-.directive('autoListDivider', function($timeout) {  
-	var lastDivideKey = "";
+.directive('autoListDivider', function($timeout) {
 
-	return {
-		link: function(scope, element, attrs) {
-			var key = attrs.autoListDividerValue;
+var lastDivideKey = "";
 
-			var defaultDivideFunction = function(k){
-				return k.slice( 0, 1 ).toUpperCase();
-			}
-      
-			var doDivide = function(){
-				var divideFunction = scope.$apply(attrs.autoListDividerFunction) || defaultDivideFunction;
-				var divideKey = divideFunction(key);
-				
-				if(divideKey != lastDivideKey) { 
-					var contentTr = angular.element("<div class='item item-divider'>"+divideKey+"</div>");
-					element[0].parentNode.insertBefore(contentTr[0], element[0]);
-				}
+return {
+    link: function(scope, element, attrs) {
+        var key = attrs.autoListDividerValue;
 
-				lastDivideKey = divideKey;
-			}
-		  
-			$timeout(doDivide,0)
-		}
-	}
+        var dividers = document.getElementsByClassName("item-divider");
+
+        while(dividers.length > 0)
+        {
+            dividers[0].parentNode.removeChild(dividers[0]);
+        }
+
+        var defaultDivideFunction = function(k){
+            return k.slice( 0, 1 ).toUpperCase();
+        }
+
+        var doDivide = function(){
+            var divideFunction = scope.$apply(attrs.autoListDividerFunction) || defaultDivideFunction;
+            var divideKey = divideFunction(key);
+
+            if(divideKey != lastDivideKey) { 
+                var contentTr = angular.element("<div class='item item-divider'>"+divideKey+"</div>");
+                element[0].parentNode.insertBefore(contentTr[0], element[0]);
+            }
+
+            lastDivideKey = divideKey;
+        }
+
+        $timeout(doDivide,0)
+    }
+}
 });
